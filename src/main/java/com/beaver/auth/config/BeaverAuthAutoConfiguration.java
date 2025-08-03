761f11e77d5880574c28f767c1d5554069f82ac1
Connor Burge <connor.d.burge@gmail.com>
2025-08-03 11:33:20 -0400

    added gateway filter

--- src/main/java/com/beaver/auth/config/BeaverAuthAutoConfiguration.java
+++ src/main/java/com/beaver/auth/config/BeaverAuthAutoConfiguration.java
@@ -2,13 +2,15 @@ package com.beaver.auth.config;
 
 import com.beaver.auth.filter.GatewaySecretFilter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.beaver.auth.filter")
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+@ConditionalOnClass(name = "jakarta.servlet.Filter")
 public class BeaverAuthAutoConfiguration {
 
     @Bean
