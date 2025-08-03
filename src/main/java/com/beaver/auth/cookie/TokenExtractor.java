868b070b738ea583e52c0e07a97112076d2845f7
Connor Burge <connor.d.burge@gmail.com>
2025-08-03 12:27:29 -0400

    added cookie stuff

--- src/main/java/com/beaver/auth/cookie/TokenExtractor.java
+++ src/main/java/com/beaver/auth/cookie/TokenExtractor.java
@@ -2,8 +2,6 @@ package com.beaver.auth.cookie;
 
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 
 @Component
 public class TokenExtractor {
@@ -24,36 +22,6 @@ public class TokenExtractor {
     public String extractRefreshToken(ServerHttpRequest request) {
         if (request.getCookies().containsKey(AuthCookieService.REFRESH_TOKEN_COOKIE)) {
             return request.getCookies().getFirst(AuthCookieService.REFRESH_TOKEN_COOKIE).getValue();
-        }
-        return null;
-    }
-
-    /**
-     * Extract access token from Servlet HttpServletRequest (for Services)
-     */
-    public String extractAccessToken(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (AuthCookieService.ACCESS_TOKEN_COOKIE.equals(cookie.getName())) {
-                    return cookie.getValue();
-                }
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Extract refresh token from Servlet HttpServletRequest (for Services)
-     */
-    public String extractRefreshToken(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (AuthCookieService.REFRESH_TOKEN_COOKIE.equals(cookie.getName())) {
-                    return cookie.getValue();
-                }
-            }
         }
         return null;
     }
